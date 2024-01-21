865f338e777aff866772faaf52432fac821fcd83
aryanjain <aryanjain1997@gmail.com>
2024-01-21 19:17:27 +0530

    commented code

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
-const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+// const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 
-module.exports = (phase) => {
-  // if (phase === PHASE_DEVELOPMENT_SERVER) {
-  //   return {
-  //     env: {
-  //       mongodb_username: "",
-  //       mongodb_password: "",
-  //       mongodb_cluster: "",
-  //       mongodb_database: "",
-  //     },
-  //   };
-  // }
+// module.exports = (phase) => {
+//   if (phase === PHASE_DEVELOPMENT_SERVER) {
+//     return {
+//       env: {
+//         mongodb_username: "",
+//         mongodb_password: "",
+//         mongodb_cluster: "",
+//         mongodb_database: "",
+//       },
+//     };
+//   }
 
-  // return {
-  //   env: {
-  //     mongodb_username: "",
-  //     mongodb_password: "",
-  //     mongodb_cluster: "",
-  //     mongodb_database: "",
-  //   },
-  // };
-};
+//   return {
+//     env: {
+//       mongodb_username: "",
+//       mongodb_password: "",
+//       mongodb_cluster: "",
+//       mongodb_database: "",
+//     },
+//   };
+// };
